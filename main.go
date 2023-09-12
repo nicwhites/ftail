9d0a9459a471587ac7393352a14553bcbce5cc73
Nic Whites <nicwhites@hotmail.com>
2023-09-11 20:29:56 -0600

    updated code to only print up to newline

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -8,17 +10,17 @@ import (
 	"regexp"
 	"syscall"
 	"time"
-	"flag"
 )
 
-const USAGE = "Usage: ftail [-delay <time_delay>] <filename> <regex_pattern>\n" 
-const BUFFER_SIZE = 4096
+const USAGE = "Usage: ftail [-delay <time_delay>] <regex_pattern> <filename>\n" 
 
 func main() {
 
+    // Flags 
+	var delay time.Duration
 	var help bool
 
-    delay := flag.Int("delay", 100, "The time delay (ms) between file reads. Minimum is 1.")
+	flag.DurationVar(&delay, "delay", 100*time.Millisecond, "The time delay between file reads (ms)")
 	flag.BoolVar(&help, "h", false, "Display program usage")
 
 	flag.Parse()
@@ -35,13 +37,8 @@ func main() {
 	}
 
 	regexPattern := flag.Arg(0)
-    filename := flag.Arg(1)
-
-    if (*delay <= 0 ) {
-        *delay = 1
-    }
-
-    time_delay := time.Duration(*delay)*time.Millisecond
+	filename := flag.Arg(1)
+    // end of flags
 
 	regex, err := regexp.Compile(regexPattern)
 	if err != nil {
@@ -50,57 +47,74 @@ func main() {
 	}
 
 	file, err := os.Open(filename)
+
 	if err != nil {
-		fmt.Fprintf(os.Stderr,"Error opening the file: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error opening the file: %v\n", err)
 		os.Exit(1)
 	}
+
 	defer file.Close()
 
 	fileInfo, err := file.Stat()
 	if err != nil {
-		fmt.Fprintf(os.Stderr,"Error getting file info: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error getting file info: %v\n", err)
 		os.Exit(1)
 	}
+
 	initialSize := fileInfo.Size()
 	currentPosition := initialSize
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
 
+	reader := bufio.NewReader(file)
+
 	go func() {
-		buffer := make([]byte, BUFFER_SIZE) 
 		for {
 			select {
 			case <-interrupt:
-				fmt.Println("Received termination signal. Exiting...")
 				return
 			default:
 				fileInfo, err := file.Stat()
 				if err != nil {
-					fmt.Printf("Error getting file info: %v\n", err)
-					time.Sleep(time.Second) 
+					fmt.Fprintf(os.Stderr, "Error getting file info: %v\n", err)
+					time.Sleep(delay)
+                    os.Exit(1)
+				}
+
+				currentSize := fileInfo.Size()
+				if currentSize < currentPosition {
+					file.Close()
+					f, err := os.Open(filename)
+                    file = f
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "Error reopening the file: %v\n", err)
+						os.Exit(1)
+					}
+
+					reader = bufio.NewReader(file)
+					currentPosition = currentSize
 					continue
 				}
 
-				if fileInfo.Size() > currentPosition {
-					n, err := file.ReadAt(buffer, currentPosition)
+				if currentSize > currentPosition {
+					newData, err := reader.ReadString('\n')
 					if err != nil && err != io.EOF {
-						fmt.Printf("Error reading file: %v\n", err)
-						time.Sleep(time.Second) 
+						fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+						time.Sleep(delay)
 						continue
 					}
 
-					if n > 0 {
-						newData := buffer[:n]
-						currentPosition += int64(n)
+					if len(newData) > 0 {
+						if regex.MatchString(newData) {
+							fmt.Printf("Matching data: %s", newData)
 
-						if regex.Match(newData) {
-							fmt.Printf("%s", newData)
+							currentPosition = currentSize
 						}
 					}
 				}
 
-				time.Sleep(time_delay) 
+				time.Sleep(delay)
 			}
 		}
 	}()
